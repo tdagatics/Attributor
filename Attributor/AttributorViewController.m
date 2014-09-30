9c77d32f58bd27c8b0f9cf7b145ab346d81fa48f
Anthony Dagati <tdagati.cs@gmail.com>
2014-09-30 11:14:32 -0700

    Initial commit of Attributor

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -10,15 +10,37 @@
 
 @interface AttributorViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+
 @end
 
 @implementation AttributorViewController
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
+    [self.body resignFirstResponder];
+    
+}
+
+- (IBAction)outlineBodySelection:(id)sender {
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3, NSStrokeColorAttributeName : [UIColor blackColor]} range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection:(id)sender {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
